285a7847056b6070d2bd177908f4ef0a500edc30
erfan sardari <sardaridats@gamil.com>
2024-12-10 13:52:16 +0330

    expensesList is created

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import ExpensesList from "./components/expensesList/components/ExpensesList";
+
+const App = () => {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "asdaf" },
+    { id: 2, description: "ccc", amount: 10, category: "asdaf" },
+    { id: 3, description: "bbb", amount: 10, category: "asdaf" },
+    { id: 4, description: "ggg", amount: 10, category: "asdaf" },
+  ]);
+  return (
+    <>
+      <ExpensesList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((del) => del.id !== id))}
+      />
+    </>
+  );
+};
+
+export default App;
